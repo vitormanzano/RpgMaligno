5af8fe24701c4e393365dc11677e3450578b0b6b
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-14 08:34:01 -0300

    feat: Creating project structure and adding the abstract class Personagem
    creating class Mago, Arqueiro and Guerreiro, all extends Personagem

---
+++ RpgMaligno/src/models/Personagem.java
@@ -0,0 +1,10 @@
+package models;
+
+public abstract class Personagem {
+    private String nome;
+    private byte pontosVida;
+    private byte ataque;
+    private byte defesa;
+    private byte nivel;
+    private int inventario;
+}
