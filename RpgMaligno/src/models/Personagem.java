fc535bce40f0fc38cbbccf917fd53f16a89f9371
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-15 22:02:08 -0300

    feat: Adding getters and setters to personagem class

--- RpgMaligno/src/models/Personagem.java
+++ RpgMaligno/src/models/Personagem.java
@@ -7,4 +7,52 @@ public abstract class Personagem {
     private byte defesa;
     private byte nivel;
     private int inventario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public byte getPontosVida() {
+        return pontosVida;
+    }
+
+    public void setPontosVida(byte pontosVida) {
+        this.pontosVida = pontosVida;
+    }
+
+    public byte getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(byte ataque) {
+        this.ataque = ataque;
+    }
+
+    public byte getDefesa() {
+        return defesa;
+    }
+
+    public void setDefesa(byte defesa) {
+        this.defesa = defesa;
+    }
+
+    public byte getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(byte nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(int inventario) {
+        this.inventario = inventario;
+    }
 }
