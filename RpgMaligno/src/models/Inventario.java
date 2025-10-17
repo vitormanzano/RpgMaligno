1d71214541f4c2e4bda4c95e95e3c7eee1e045df
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-17 13:52:33 -0300

    feat: Adding methods to Inventario clas
    s

--- RpgMaligno/src/models/Inventario.java
+++ RpgMaligno/src/models/Inventario.java
@@ -1,4 +1,22 @@
 package models;
 
 public class Inventario {
+    public Item[] itens;
+
+    public void adicionarItem(Item item)
+    {
+
+    }
+
+    public void removerItem(Item item)
+    {
+
+    }
+
+    public void listarItens()
+    {
+
+    }
+
+    
 }
