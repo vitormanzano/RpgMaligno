1b7600db21ccf373d95b3198f0cdf37e172f68e4
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-17 14:20:16 -0300

    feat: Adding methods in Item class

--- RpgMaligno/src/models/Guerreiro.java
+++ RpgMaligno/src/models/Guerreiro.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Guerreiro extends Personagem {
+
+    public Guerreiro()
+    {
+        this.setPontosVida((byte) 75);
+        this.setAtaque((byte) 10);
+        this.setDefesa((byte) 10);
+
+    }
 }
