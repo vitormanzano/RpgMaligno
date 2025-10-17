d88b354e0d6d084b5c917c4bfbf30b3cf7f9e8c3
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-17 14:59:34 -0300

    feat: Adding methods in Inventario class Adding toString, equals and hashcode to Inventario class

--- RpgMaligno/src/models/Inventario.java
+++ RpgMaligno/src/models/Inventario.java
@@ -3,20 +3,43 @@ package models;
 public class Inventario {
     public Item[] itens;
 
-    public void adicionarItem(Item item)
-    {
+    public void adicionarItem(Item item) {
 
     }
 
-    public void removerItem(Item item)
-    {
+    public void removerItem(Item item) {
 
     }
 
-    public void listarItens()
-    {
+    public void listarItens() {
 
     }
+    @Override
+    public String toString() {
+        for (Item item : itens) {
+            System.out.println(item);
+        }
+        return "";
+    }
 
-    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (this.getClass() != obj.getClass()) return false;
+        Inventario other = (Inventario) obj;
+        for (int i = 0; i < itens.length; i++) {
+            if (!itens[i].equals(other.itens[i])) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 1;
+        for (Item item : itens) {
+            hash = hash * 7 + item.hashCode();
+        }
+        return hash;
+    }
 }
