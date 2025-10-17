fda3a155597b963b0ae293b735bd5c588979dfed
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-16 22:49:42 -0300

    feat: Adding equals, toString and hashcode to Personagem class

--- RpgMaligno/src/models/Personagem.java
+++ RpgMaligno/src/models/Personagem.java
@@ -6,7 +6,7 @@ public abstract class Personagem {
     private byte ataque;
     private byte defesa;
     private byte nivel;
-    private int inventario;
+    private Inventario inventario;
 
     public String getNome() {
         return nome;
@@ -48,11 +48,53 @@ public abstract class Personagem {
         this.nivel = nivel;
     }
 
-    public int getInventario() {
+    public Inventario getInventario() {
         return inventario;
     }
 
-    public void setInventario(int inventario) {
+    public void setInventario(Inventario inventario) {
         this.inventario = inventario;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + this.nome + "\n" +
+                "Nível: " + this.nivel + "\n" +
+                "HP: " + this.pontosVida + "\n" +
+                "Ataque: " + this.ataque + "\n" +
+                "Defesa: " + this.defesa + "\n" +
+                "Inventário: " + this.inventario;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (this.getClass() != obj.getClass()) return false;
+
+        Personagem other = (Personagem) obj;
+        if (!this.nome.equals(other.nome) ||
+                this.pontosVida != other.pontosVida ||
+                this.ataque != other.ataque ||
+                this.defesa != other.defesa ||
+                this.nivel != other.nivel ||
+                this.inventario.equals(other.inventario)) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 1;
+
+        hash = hash * 7 + (nome.hashCode());
+        hash = hash * 7 + ((Byte)this.pontosVida).hashCode();
+        hash = hash * 7 + ((Byte)this.ataque).hashCode();
+        hash = hash * 7 + ((Byte)this.defesa).hashCode();
+        hash = hash * 7 + ((Byte)this.nivel).hashCode();
+        hash = hash * 7 + inventario.hashCode();
+        
+        if (hash < 0) hash = -hash;
+
+        return hash;
     }
 }
