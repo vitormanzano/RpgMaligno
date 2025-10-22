b0d6e3b1b2dcb35d15e97830d08952669598a8d8
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-22 12:43:10 -0300

    fix: Tirando modelos das classes de jogadro
    Arrumando conflict

---
+++ RpgMaligno/src/Itens/PocaoCura.java
@@ -0,0 +1,9 @@
+package Itens;
+
+import models.Item;
+
+public class PocaoCura extends Item {
+    public PocaoCura() {
+        super("Poção de cura", "Quando o jogador usar essa poção sua vida aumenta", "A vida do jogador aumenta em 15 pontos", (byte)1);
+    }
+}
