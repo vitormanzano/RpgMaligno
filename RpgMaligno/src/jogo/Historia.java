2ca40b49b88de85f4bd9c28dae0522ee8579ba2a
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-10-23 19:48:59 -0300

    feat: Estruturando história | Criando introdução.

---
+++ RpgMaligno/src/jogo/Historia.java
@@ -0,0 +1,25 @@
+package jogo;
+
+public class Historia {
+    public static void printIntroducao() {
+        System.out.println("Neon Heist. Ano: 2197. Nightgird - uma cidade onde o céu é feito de fumaça e o ch±ao de corpos esquecidos. As megacorporações controlam tudo: governo, informação e até o ar que as pessoas respiram.");
+        Jogo.pressAnythingToContinue();
+        System.out.println("Os ricos vivem acima das nuvens. O resto... luta para não morrer de fome no nível da rua. Você nasceu nesse resto");
+        Jogo.pressAnythingToContinue();
+        System.out.println("Sua mãe era socorrista, uma das poucas que ainda acreditavam em salvar vidas. Corria de ambulância pelos becos e zonas de guerra, tentando remendar o que sobrava da humanidade. Mas a verdade é que salvar pessoas não paga as contas.");
+        Jogo.pressAnythingToContinue();
+        System.out.println("Então, quando ninguém olhava, ela fazia o que precisava: roubava implantes, próteses e chips de valor dos corpos dos mortos. Contrabando sujo, mas necessário pra manter você vivo.");
+        Jogo.pressAnythingToContinue();
+        System.out.println("Uma noite, ela chegou em casa exausta, com sangue nos braços e o olhar distante. Jogou a bolsa num canto e disse apenas: “Hoje... encontrei algo que pode mudar nossas vidas.” Você não perguntou o que era. ");
+        Jogo.pressAnythingToContinue();
+        System.out.println("No dia seguinte, a chamada de emergência veio: incidente com tiroteio em área corporativa. Ela atendeu como sempre. Só que dessa vez... não voltou. ");
+        Jogo.pressAnythingToContinue();
+        System.out.println(" relatório dizia: fogo cruzado entre gangues e segurança privada. A ambulância foi atingida. Nenhum sobrevivente. Quando você foi buscar os pertences dela, a dor era mais pesada que o silêncio.");
+        Jogo.pressAnythingToContinue();
+        System.out.println("No meio da bolsa, encontrou um objeto escondido no fundo falso — um equipamento raro, proibido... e perigoso. Talvez fosse o mesmo que ela quis vender.");
+        Jogo.pressAnythingToContinue();
+        System.out.println("Talvez tenha sido por causa dele que morreu. Agora, é tudo o que resta dela. E tudo o que pode te dar uma chance de sobreviver.");
+        Jogo.pressAnythingToContinue();
+        System.out.println("Escolha o legado deixado por sua mãe Cada item parece pulsar com energia — como se te escolhesse de volta.");
+    }
+}
--- RpgMaligno/src/jogo/Jogo.java
+++ RpgMaligno/src/jogo/Jogo.java
@@ -1,5 +1,22 @@
 package jogo;
 
-public class Jogo {
+import java.util.Scanner;
 
+public class Jogo {
+    static Scanner scanner = new Scanner(System.in);
+
+    private byte atoAtual;
+
+
+    public static void iniciarJogo() 
+    {
+        System.out.println("Bem vindo ao seu Rpg maligno...");
+
+    }
+
+
+    public static void pressAnythingToContinue() {
+        System.out.println("Pressione qualquer coisa para continuar...");
+        scanner.nextLine();
+    }
 }
